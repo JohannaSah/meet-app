1d5b4e807144977dafeef358c9b735c1ba3bf482
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-08 11:31:24 +0100

    imported and used EventList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import EventList from './EventList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <EventList />
       </div>
     );
   }
