b6b5e76c13150d21495acf2f335f2074c7bd5ca3
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-13 16:38:09 +0100

    add: more integration tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -27,23 +27,31 @@ describe('<App /> integration', () => {
         const AppWrapper = mount(<App />);
         const AppEventsState = AppWrapper.state('events');
         expect(AppEventsState).not.toEqual(undefined);
-        expect(AppEventsState.find(EventList).props().events).toEqual(AppEventsState);
+        expect(AppWrapper.find(EventList).props().events).toEqual(AppEventsState);
         AppWrapper.unmount();
     });
 
-    test('get list of events matching city selected by user', async () => {
+    test('App passes "locations" state as a prop to CitySearch', () => {
+        const AppWrapper = mount(<App />);
+        const AppLocationsState = AppWrapper.state('locations');
+        expect(AppLocationsState).not.toEqual(undefined);
+        expect(AppWrapper.find(CitySearch).props().locations).toEqual(AppLocationsState);
+        AppWrapper.unmount();
+    });
+
+    test('get list of events matching the city selected by the user', async () => {
         const AppWrapper = mount(<App />);
         const CitySearchWrapper = AppWrapper.find(CitySearch);
         const locations = extractLocations(mockData);
         CitySearchWrapper.setState({ suggestions: locations });
-        const suggestions = CitySearchWrapper.state('suggestions'); 
+        const suggestions = CitySearchWrapper.state('suggestions');
         const selectedIndex = Math.floor(Math.random() * (suggestions.length));
         const selectedCity = suggestions[selectedIndex];
         await CitySearchWrapper.instance().handleItemClicked(selectedCity);
         const allEvents = await getEvents();
-        const eventsToShow = allEvents.fileter(event => event-location === selectedCity);
+        const eventsToShow = allEvents.filter(event => event.location === selectedCity);
         expect(AppWrapper.state('events')).toEqual(eventsToShow);
-        AppWrapper(unmount);
+        AppWrapper.unmount();
     });
 
 });
