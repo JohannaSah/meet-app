f6d24cdca97d3a28ca317ff9bd279582432bb390
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-14 13:30:18 +0100

    Update CitySearch.js

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -51,6 +51,7 @@ class CitySearch extends Component {
               value={this.state.query}
               onChange={this.handleInputChanged}
               onFocus={ this.handleInputFocus}
+              placeholder="Search for a city"
             />
             
             <ul 
