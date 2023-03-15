2514dc0a217c66f0308ebf5c004610d24f5aeea5
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-15 14:31:03 +0100

    created alerts through OOP

---
+++ src/Alert.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+  constructor(props) {
+    super(props);
+    this.color = null;
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+    };
+  }
+
+  render() {
+    return (
+      <div className="Alert">
+        <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
+    );
+  }
+}
+
+class InfoAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'blue';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontStyle: 'italic',
+    };
+  }
+
+}
+
+class ErrorAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'red';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontStyle: 'bold',
+    };
+  }
+}
+
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'orange';
+  }
+
+  getStyle = () => {
+    return {
+      color: this.color,
+      fontStyle: 'bold',
+    };
+  }
+}
+
+export { InfoAlert, ErrorAlert, WarningAlert };
