eb56dca173bb8707fe4445a7ce4cadbb4f5d9a9c
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-26 17:49:00 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -56,31 +56,37 @@ const removeQuery = () => {
 export const getEvents = async () => {
     NProgress.start();
 
-    if (isLocalhost) {
+    if (window.location.href.indexOf('localhost') > -1) {
         NProgress.done();
         return mockData;
-      }
+    };
     
-    if (!navigator.onLine) {
-    const data = localStorage.getItem("lastEvents");
-    NProgress.done();
-    return data ? JSON.parse(data).events : [];
-    }
-
-   const token = await getAccessToken();
-
-   if (token) {
-    removeQuery();
-    const url = 'https://7vhpofmv4m.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
-    const result = await axios.get(url);
     if (result.data) {
         var locations = extractLocations(result.data.events);
         localStorage.setItem('lastEvents', JSON.stringify(result.data));
         localStorage.setItem('locations', JSON.stringify(locations));
+    };
+    
+    if (!navigator.onLine) {
+        const data = localStorage.getItem("lastEvents");
         NProgress.done();
-        return result.data.events;
-    }
-   };
+        return data ? JSON.parse(events).events:[];
+    };
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = 'https://7vhpofmv4m.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const result = await axios.get(url);
+        if (result.data) {
+            var locations = extractLocations(result.data.events);
+            localStorage.setItem('lastEvents', JSON.stringify(result.data));
+            localStorage.setItem('locations', JSON.stringify(locations));
+            NProgress.done();
+            return result.data.events;
+        }
+    };
 };
 
 export const getAccessToken = async () => {
