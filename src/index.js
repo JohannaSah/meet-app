6c2c424aca0e9e8bd3261e216ea46bc38cbf7779
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-26 15:40:21 +0200

    test 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { render } from "react-dom";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
