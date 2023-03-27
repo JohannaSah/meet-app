eeea7757ffb6645f1b7f9135010d4c7c1f518575
JohannaS <109289350+JohannaSah@users.noreply.github.com>
2023-03-27 15:22:22 +0200

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -26,7 +26,7 @@ const EventGenre = ({ events }) => {
         <ResponsiveContainer height={400}>
             <PieChart width={400} height={400}>
                 <Pie
-                    data={this.getData()}
+                    data={this.data}
                     cx={200}
                     cy={200}
                     labelLine={false}
